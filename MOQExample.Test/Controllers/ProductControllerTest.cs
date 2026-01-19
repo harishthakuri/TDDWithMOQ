bea9e02da0b35105096da4b84d46cfbc29ea8cf0
Harish <codemaverick80@gmail.com>
2026-01-19 11:56:00 -0800

    updated test suites

--- MOQExample.Test/Controllers/ProductControllerTest.cs
+++ MOQExample.Test/Controllers/ProductControllerTest.cs
@@ -1,23 +1,19 @@
-﻿using FluentValidation;
+﻿using FluentAssertions;
+using FluentValidation;
 using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using MOQCoreAPI.Controllers;
 using MOQCoreAPI.Domain;
-using MOQCoreAPI.Exceptions;
 using MOQCoreAPI.Repositories;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
-////https://stackoverflow.com/questions/66307477/how-to-verify-iloggert-log-extension-method-has-been-called-using-moq
-////https://chrissainty.com/unit-testing-ilogger-in-aspnet-core/
 namespace MOQExample.Test.Controllers
 {
+
     public class ProductControllerTest
     {
         readonly ProductController controller;
@@ -25,164 +21,26 @@ namespace MOQExample.Test.Controllers
         private readonly Mock<ILogger> _loggerMock = new Mock<ILogger>();
         private readonly ProductValidator _validator =new ProductValidator();
 
-        private readonly Random _random = new Random();
-
 
         public ProductControllerTest()
         {
             controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
         }
 
-        ////////naming convention UnitOfWork_SateUnderTest_ExpectedResult()
-        ////[Fact]
-        ////// public void GetById_WhenProductExists_ShouldReturnProduct()
-        ////public void GetById_ShouldReturnProduct_WhenProductExists()
-        ////{
-        ////    ////Arrange
-
-        ////    var rndProduct=CreateRandomProduct();
-
-        ////    _productRepoMock.Setup(p => p.Get(rndProduct.Id)).Returns(rndProduct);
-        ////    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-
-        ////    ////Act
-        ////    var actual = controller.GetById(rndProduct.Id);
-
-        ////    ////Assert
-        ////   // Assert.Same(product, actual);
-        ////    Assert.Equal(rndProduct.Id, actual.Id);
-        ////    //Assert.Equal(name, actual.Name);
-        ////}
 
 
-
-        ////[Fact]
-        ////public void GetById_ShouldThrowInvalidIdException_WhenProductIdIsZeroOrLess()
-        ////{
-        ////    ////Arrange
-        ////    var id = 12;
-        ////    Expression<Action<ILogger>> expression = x => x.Log(
-        ////        It.IsAny<LogLevel>(),
-        ////        It.IsAny<EventId>(),
-        ////        It.IsAny<It.IsAnyType>(),
-        ////        It.IsAny<Exception>(),
-        ////        (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()
-        ////    );
-
-        ////    _loggerMock.Setup(expression).Verifiable();
-        ////    _productRepoMock.Setup(p => p.Get(id)).Throws(new InvalidGuidException(nameof(Product), id));
-        ////    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-
-        ////    ////Act and Assert
-        ////    var result = Assert.Throws<InvalidGuidException>(() => controller.GetById(id));
-        ////    _loggerMock.Verify(expression, Times.Once);
-
-        ////    ////OR
-        ////    //var result = Assert.Throws<InvalidGuidException>(() => controller.GetById(id));
-        ////    //var exception = Assert.IsType<InvalidGuidException>(result);
-        ////    //Assert.Equal(exception.Message, result.Message);
-        ////    //Assert.Equal(exception.Reason, result.Reason);
-        ////}
-
-
-        ////[Fact]
-        ////public void GetById_ShouldLogException_WhenProductDosNotExists()
-        ////{
-        ////    ////Arrange
-
-        ////    var product = new Product { Id = 12, Name = "Book", Price = 122 };
-
-
-        ////    var id = 12;
-        ////    Expression<Action<ILogger>> expression = x => x.Log(
-        ////        It.IsAny<LogLevel>(),
-        ////        It.IsAny<EventId>(),
-        ////        It.IsAny<It.IsAnyType>(),
-        ////        It.IsAny<Exception>(),
-        ////        (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()
-        ////    );
-
-        ////    _loggerMock.Setup(expression).Verifiable();
-
-        ////    _productRepoMock.Setup(p => p.Get(id)).Throws<NotFoundException>();
-        ////    //_productRepoMock.Setup(p => p.Get(id)).Returns(product);
-
-        ////    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-
-        ////    ////Act
-        ////    Assert.Throws<NotFoundException>(() => controller.GetById(12));
-
-        ////    ////Assert
-        ////    _loggerMock.Verify(expression, Times.Once);
-        ////}
-
-
-        private Product CreateRandomProduct()
+        private Product CreateValidProduct()
         {
-            return new()
+            return new Product
             {
-                Id = _random.Next(10),
-                Name = $"Test Produc {_random.Next(20)}",
-                Price = _random.Next(1000)
+                Id = 1,
+                Name = "Test Product",
+                Price = 100.50,
+                Email = "test@example.com",
+                NotifactionType = "email",
+                ZipCode = "12345"
             };
-
         }
-
-
-        //[Fact]      
-        //public void AddProduct_ShouldThrowException_WhenProductNameIsNullorEmpty()
-        //{
-        //    ////// Arrange
-        //    var product = new Product { Id = 45, Name = "", Price = 122 };
-        //    // _productRepoMock.Setup(p => p.AddProduct(product)).Returns(true);
-        //    _productRepoMock.Setup(p => p.AddProduct(product)).Throws<ArgumentNullException>();
-
-        //    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-
-        //    ////// Act
-
-        //    ////// Assert
-        //    Assert.Throws<ArgumentNullException>(()=>controller.AddProduct(product));
-
-
-        //}
-
-        //[Fact]
-        //public void AddProduct_ShouldThrowException_WhenProductIdIsLessThenZero()
-        //{
-        //    ////// Arrange
-        //    var product = new Product { Id = 0, Name = "Book", Price = 122 };
-        //    //_productRepoMock.Setup(p => p.AddProduct(product)).Returns(true);
-        //    _productRepoMock.Setup(p => p.AddProduct(product)).Throws<ArgumentOutOfRangeException>();
-        //    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-
-        //    ////// Act
-
-        //    ////// Assert
-        //    Assert.Throws<ArgumentOutOfRangeException>(() => controller.AddProduct(product));
-
-
-        //}
-
-        //[Fact]
-        //public void AddProduct_ShouldReturnTrue_WhenProductAddedSuccessfully()
-        //{
-        //    ////Arrange
-        //    var product = new Product { Id = 4, Name = "iPad 12 Pro", Price = 1500 };
-
-        //    _productRepoMock.Setup(p => p.AddProduct(product)).Returns(true);
-
-        //    var controller = new ProductController(_productRepoMock.Object, _loggerMock.Object);
-        //    ////Act
-        //    var actual = controller.AddProduct(product);
-
-        //    ////Assert
-        //    Assert.True(actual);
-
-        //}
-
-
-
 
         [Fact]
         public void AddProduct_ReturnBadRequestResult_WhenModelIsInvalid()
@@ -191,10 +49,10 @@ namespace MOQExample.Test.Controllers
             // Arrange
             var product = new Product {
                 Id = 1,
-                Name = "Harish Chand",
+                Name = "", // Invalid: empty name to trigger validation error
                 Price = 122,
-                NotifactionType="text",
-                ZipCode="df",
+                NotifactionType="invalid", // Invalid: doesn't match pattern "push|text|email|dial"
+                ZipCode="98765",
                 Email="abc@hotmail.com"
             };
 
@@ -212,6 +70,106 @@ namespace MOQExample.Test.Controllers
            
         }
 
+        [Fact]
+        public void AddProduct_ReturnsOkResult_WhenProductIsValid()
+        {
+            // Arrange
+            var product = CreateValidProduct();
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>())).Returns(true);
+
+            // Act
+            var result = controller.AddProduct(product);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().Be(true);
+            _productRepoMock.Verify(x => x.AddProduct(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddProduct_CallsRepositoryWithCorrectProduct()
+        {
+            // Arrange
+            var product = CreateValidProduct();
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>())).Returns(true);
+
+            // Act
+            controller.AddProduct(product);
+
+            // Assert
+            _productRepoMock.Verify(x => x.AddProduct(
+                It.Is<Product>(p => 
+                    p.Id == product.Id &&
+                    p.Name == product.Name &&
+                    p.Price == product.Price &&
+                    p.Email == product.Email &&
+                    p.NotifactionType == product.NotifactionType &&
+                    p.ZipCode == product.ZipCode
+                )), Times.Once);
+        }
+
+        [Fact]
+        public void AddProduct_ReturnsOkResult_WhenRepositoryReturnsFalse()
+        {
+            // Arrange
+            var product = CreateValidProduct();
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>())).Returns(false);
+
+            // Act
+            var result = controller.AddProduct(product);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().Be(false);
+            _productRepoMock.Verify(x => x.AddProduct(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddProduct_ThrowsException_WhenRepositoryThrowsArgumentNullException()
+        {
+            // Arrange
+            var product = CreateValidProduct();
+            var exceptionMessage = "Product name can not be null or empty";
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>()))
+                .Throws(new ArgumentNullException(exceptionMessage));
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => controller.AddProduct(product));
+            exception.Message.Should().Contain(exceptionMessage);
+            _productRepoMock.Verify(x => x.AddProduct(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddProduct_ThrowsException_WhenRepositoryThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            var product = CreateValidProduct();
+            var exceptionMessage = "Product id should be greater then 0";
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>()))
+                .Throws(new ArgumentOutOfRangeException(exceptionMessage));
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => controller.AddProduct(product));
+            exception.Message.Should().Contain(exceptionMessage);
+            _productRepoMock.Verify(x => x.AddProduct(It.IsAny<Product>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddProduct_HandlesNullProduct()
+        {
+            // Arrange
+            Product product = null;
+            _productRepoMock.Setup(x => x.AddProduct(It.IsAny<Product>())).Returns(false);
+
+            // Act
+            var result = controller.AddProduct(product);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            _productRepoMock.Verify(x => x.AddProduct(It.Is<Product>(p => p == null)), Times.Once);
+        }
 
 
     }
